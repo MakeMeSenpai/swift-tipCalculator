d2cd1ce661dc98b568a23b4341ed05c92b8cf73e
Anthony <sean.a.protho1@gmail.com>
2021-07-08 20:40:42 -0700

    implimented subviews

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -10,10 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // header
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    // input
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    // output
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    // footer
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // header
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch toggled on")
+        } else {
+            print("switch toggled off")
+        }
+    }
+    
+    // input
+    @IBAction func tipPercentChanged(_ sender: UISegmentedControl) {
+    }
+    
+    // output
+    
+    // footer
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        print("reset button tapped")
+    }
 }
-
